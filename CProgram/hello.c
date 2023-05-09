e10cd4108b81240852bceed1fe34b152092b2d9a
Megha <meghapatel.tops@gmail.com>
2023-05-09 09:58:12 +0530

    hello print

---
+++ CProgram/hello.c
@@ -0,0 +1,11 @@
+#include<stdio.h>//std input output header file
+#include<conio.h>//console input output header file
+main(){
+	//to print hello..
+	/*
+	multiple lines comment
+	*/
+	printf("Hello World");
+	printf("\nName:Megha\tPatel");
+	printf("\n    ****");
+}
